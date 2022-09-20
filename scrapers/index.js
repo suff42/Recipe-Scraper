3776371b4642c323affa4c6183417b80b758666c
Radek <radek.rpiotrowski@gmail.com>
2022-09-20 10:05:52 +0200

    test code to validate new requirements

--- scrapers/index.js
+++ scrapers/index.js
@@ -2,9 +2,20 @@
 
 const ScraperFactory = require("../helpers/ScraperFactory");
 
-const recipeScraper = async url => {
+const recipeScraper = async (url) => {
   let klass = new ScraperFactory().getScraper(url);
   return await klass.fetchRecipe();
 };
 
 module.exports = recipeScraper;
+
+const url =
+  "https://www.jamieoliver.com/recipes/pancake-recipes/crispy-rice-pancakes/";
+
+recipeScraper(url)
+  .then((recipe) => {
+    console.log(recipe);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
