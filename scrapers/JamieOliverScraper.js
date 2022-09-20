30d57b05d4cca0b081716ae67607aae3d67365c4
Radek <radek.rpiotrowski@gmail.com>
2022-09-20 09:28:58 +0200

    update list of arrays and scrape subName

--- scrapers/JamieOliverScraper.js
+++ scrapers/JamieOliverScraper.js
@@ -9,37 +9,30 @@ class JamieOliverScraper extends PuppeteerScraper {
 
   scrape($) {
     this.defaultSetImage($);
-    const { ingredients, instructions, time, tags } = this.recipe;
+    const { specialDiets, tags, ingredients, instructions } = this.recipe;
     this.recipe.name = $(".single-recipe-details h1").text();
+    this.recipe.subName = $(".single-recipe-details p").text();
+
     this.recipe.description = $("meta[property='og:description']").attr(
       "content"
     );
 
     $(".tags-list a").each((i, el) => {
-      const tag = $(el)
-        .text()
-        .replace(/\s\s+/g, " ")
-        .trim();
+      const tag = $(el).text().replace(/\s\s+/g, " ").trim();
       if (tag !== "") {
         tags.push(tag);
       }
     });
 
     $("ul.ingred-list li").each((i, el) => {
-      const item = $(el)
-        .text()
-        .replace(/\s\s+/g, " ")
-        .trim();
+      const item = $(el).text().replace(/\s\s+/g, " ").trim();
       if (item !== "") {
         ingredients.push(item);
       }
     });
 
     $("ol.recipeSteps li").each((i, el) => {
-      const step = $(el)
-        .text()
-        .replace(/\s\s+/g, " ")
-        .trim();
+      const step = $(el).text().replace(/\s\s+/g, " ").trim();
       if (step !== "") {
         instructions.push(step);
       }
